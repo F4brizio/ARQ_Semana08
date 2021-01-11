97dd3be2d9fc3cd0f631e5d0c8cc2e946b63b933
Alexander <a.moreno02018@gmail.com>
2021-01-10 20:07:45 -0500

    logicaDeGanador

--- src/app/IJugador.java
+++ src/app/IJugador.java
@@ -2,7 +2,10 @@ package app;
 
 public interface IJugador {
 
-
+    public  int Puntaje=0;
+    public  int getPuntaje();
+    public  void setPuntaje(int p);
     public void JugadaTurno();
     public Elemento getJugada();
+
 }
